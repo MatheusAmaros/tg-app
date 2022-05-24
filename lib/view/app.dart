f72f127a2e9ac9522e9bdcf32df0e6680f5974a8
MatheusAmaros <matheusamaros@hotmail.com>
2022-05-24 14:02:49 -0300

    Adição do cadastro de guarnição completo

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tg_app/view/cadastro.view.dart';
-import 'package:tg_app/view/cadastroGuarnicao.view.dart';
 import 'package:tg_app/view/cadastroGuarnicaoCompleta.view.dart';
 import 'package:tg_app/view/chamada.view.dart';
 import 'package:tg_app/view/login.view.dart';
