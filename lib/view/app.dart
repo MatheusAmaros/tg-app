948eecb32a7d804702ce03a44b17e8dce09fe5f6
Andre Luis da Silva <andreluis2608@gmail.com>
2022-05-25 18:07:48 -0300

    Tela de Home

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -5,14 +5,18 @@ import 'package:tg_app/view/chamada.view.dart';
 import 'package:tg_app/view/chamadaPelotao.view.dart';
 import 'package:tg_app/view/login.view.dart';
 import 'package:tg_app/view/home.view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class App extends StatelessWidget {
+
+FirebaseAuth auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      initialRoute: '/login',
+      initialRoute: auth.currentUser == null ? '/login':'/home',
       routes: {
         '/login': (context) => LoginWidget(),
         '/chamadaPelotao': (context) => ChamadaPelotaoView(),
