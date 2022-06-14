a69a0b63c8435a336442fdf905552344a2a3725e
André Luis da Silva <andreluis2608@gmail.com>
2022-06-14 15:57:16 -0300

    Correçao do layout cadastros

--- lib/view/home.viewD.dart
+++ lib/view/home.viewD.dart
@@ -191,7 +191,7 @@ class _HomeState extends State<Home> {
                       IconButton(
                         icon: Icon((Icons.person)),
                         iconSize: 30,
-                        color: Colors.white,
+                        color: Colors.transparent,
                         onPressed: () {},
                       ),
                     ],
@@ -336,22 +336,7 @@ class _HomeState extends State<Home> {
                                 ],
                               ),
                               Column(
-                                children: [
-                                  Column(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      IconButton(
-                                          onPressed: () {
-                                            Navigator.pushNamed(
-                                                context, '/chamadaPelotaoV');
-                                          },
-                                          iconSize: 50,
-                                          icon: Icon(Icons.table_view)),
-                                      Text('Ver chamada')
-                                    ],
-                                  ),
-                                ],
+                                children: [Container(margin: EdgeInsets.fromLTRB(0, 0, 95, 0),)], //layout da tela
                               ),
                             ]),
                       ),
